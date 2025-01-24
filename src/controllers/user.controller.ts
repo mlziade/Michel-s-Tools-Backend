f25d57240f3b06b148b43beb2e1b50e57234453b
mlziade <ziade.michel@hotmail.com>
2025-01-23 22:42:07 -0300

    feat: update user controller to return UserResponseDto instead of User entity

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Put, Delete, UseGuards, Body, Param, Query } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserContractRequest, QueryUserPaginatedDto, UpdateUserContractRequest } from 'src/contracts/requests/user.contract.request';
+import { UserResponseDto } from 'src/contracts/response/user.contract.response';
 import { User } from 'src/entities/user.entity';
 import { ApiKeyGuard } from 'src/guards/apikey.guard';
 import { UserService } from 'src/services/user.service';
@@ -13,46 +14,46 @@ export class UserController {
     ) {}
     
     @Get()
-    async findAll() {
+    async findAll(): Promise<UserResponseDto[]> {
         const users: User[] = await this.userService.findAll();
-        return users;
+        return users.map(user => UserResponseDto.fromUser(user));
     }
 
     @Get('paginated')
     async findPaginated(
         @Query('query') query: QueryUserPaginatedDto,
-    ): Promise<User[]> {	
+    ): Promise<UserResponseDto[]> {	
         const users: User[] = await this.userService.findPaginated(query);
-        return users;
+        return users.map(user => UserResponseDto.fromUser(user));
     }
 
     @Get(':id')
-    async findById(@Param('id') id: number): Promise<User> {
+    async findById(@Param('id') id: number): Promise<UserResponseDto> {
         const existingUser: User = await this.userService.findById(id);
-        return existingUser;
+        return UserResponseDto.fromUser(existingUser);
     }
 
     @Get(':username')    
-    async findByUsername(@Param('username') username: string): Promise<User> {
+    async findByUsername(@Param('username') username: string): Promise<UserResponseDto> {
         const existingUser: User = await this.userService.findByUsername(username);
-        return existingUser;
+        return UserResponseDto.fromUser(existingUser);
     }
 
     @Post()
-    async create(@Body() user: CreateUserContractRequest): Promise<User> {
+    async create(@Body() user: CreateUserContractRequest): Promise<UserResponseDto> {
         const newUser: User = await this.userService.create(user);
-        return newUser;
+        return UserResponseDto.fromUser(newUser);
     }
 
     @Put(':id')
-    async update(@Param('id') id: number, @Body() updateDto: UpdateUserContractRequest): Promise<User> {
+    async update(@Param('id') id: number, @Body() updateDto: UpdateUserContractRequest): Promise<UserResponseDto> {
         const updatedUser: User = await this.userService.update(id, updateDto);
-        return updatedUser;
+        return UserResponseDto.fromUser(updatedUser);
     }
 
     @Delete(':id')
-    async remove(@Param('id') id: number): Promise<User> {
+    async remove(@Param('id') id: number): Promise<UserResponseDto> {
         const deletedUser: User = await this.userService.remove(id);
-        return deletedUser;
+        return UserResponseDto.fromUser(deletedUser);
     }
 }
